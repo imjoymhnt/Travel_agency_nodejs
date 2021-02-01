25aa149c79b96930bef9eb4470a0850b6d3ce1ee
Mritunjoy Mahanta <mritunjoy.mahanta001@gmail.com>
2021-02-02 02:25:36 +0530

    add mongodb database

--- app.js
+++ app.js
@@ -10,6 +10,20 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+mongoose.connect("mongodb://localhost:27017/travelsDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const carSchema = new mongoose.Schema({
+  name: String,
+  color: String,
+  owner: String,
+  phNumber: Number,
+});
+
+const Car = mongoose.model("Car", carSchema);
+
 app.get("/", (req, res) => {
   res.render("home");
 });
