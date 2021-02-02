fd9c260de39900fbd0ceab6105851bfddb229502
Mritunjoy Mahanta <mritunjoy.mahanta001@gmail.com>
2021-02-02 21:52:27 +0530

    complete homepage styling and render car data from the database

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
+// Importing the requiring modules
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const multer = require("multer");
 
 // initialize app
 const app = express();
@@ -10,11 +12,30 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+// Multer configuration for image upload
+const storage = multer.diskStorage({
+  destination: function (request, file, callback) {
+    callback(null, "./public/uploads/images");
+  },
+  filename: function (request, file, callback) {
+    callback(null, Date.now() + file.originalname);
+  },
+});
+
+const upload = multer({
+  storage: storage,
+  limits: {
+    fieldSize: 1024 * 1024 * 3,
+  },
+});
+
+// connecting to the mongodb database
 mongoose.connect("mongodb://localhost:27017/travelsDB", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
+// Creating the mongodb Schema
 const carSchema = new mongoose.Schema({
   name: String,
   color: String,
@@ -23,28 +44,43 @@ const carSchema = new mongoose.Schema({
   img: String,
 });
 
+// Creating the model for the Schema
 const Car = mongoose.model("Car", carSchema);
 
+// Home page get request
 app.get("/", (req, res) => {
-  res.render("home");
+  Car.find((err, foundCars) => {
+    if (err) {
+      res.redirect("/");
+    } else {
+      if (foundCars) {
+        res.render("home", { cars: foundCars });
+      }
+    }
+  });
 });
 
+// Create page get request
 app.get("/add", (req, res) => {
   res.render("add", { title: "Add Cars" });
 });
 
-app.post("/add", (req, res) => {
+// Handling the Post request for the add from
+app.post("/add", upload.single("image"), (req, res) => {
+  console.log(req);
   const car = new Car({
     name: req.body.title,
     color: req.body.color,
     owner: req.body.owner,
     phNumber: req.body.phNumber,
-    img: req.body.image,
+    img: req.file.filename,
   });
 
   car.save();
+  res.redirect("/add");
 });
 
+// Listining to the 3000 port
 app.listen(3000, () => {
   "Listining to the port 3000";
 });
